13e37f3bd6bdff634749033ed2841beb4248b665
mozzaru <rianyanuar52@gmail.com>
2023-04-10 13:19:51 +0700

    markw: add RemovePackages

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := /dev/null
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt Drive GoogleFeedback GooglePrintRecommendationService GoogleTTS LocationHistoryPrebuilt Maps Photos PrebuiltDeskClockGoogle PrebuiltGmail TagGoogle talkback Velvet YouTube AndroidAuto
+LOCAL_UNINSTALLABLE_MODULE := true
+include $(BUILD_PREBUILT)
