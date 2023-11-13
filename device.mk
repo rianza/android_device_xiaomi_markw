26004b699eea170a16bed551ebc8c2d86c7e12be
SonicBSV <sonicbsv@gmail.com>
2023-11-13 11:44:38 +0300

    markw: up interfaces for Android 14 U compatibility

--- device.mk
+++ device.mk
@@ -214,7 +214,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 PRODUCT_COPY_FILES += \
@@ -413,8 +413,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
     librmnetctl \
