3fdb070a1942e52780950c1cbf2c50fd15c67e67
rianza <rianyanuar53@gmail.com>
2023-11-25 06:33:04 +0700

    remove_packages: add and remove app google

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := NfcNci AudioFX
+LOCAL_OVERRIDES_PACKAGES := NfcNci AudioFX CalculatorGooglePrebuilt CalendarGooglePrebuilt Drive GoogleFeedback GooglePrintRecommendationService GoogleTTS Maps Photos PrebuiltDeskClockGoogle PrebuiltGmail TagGoogle talkback Velvet AndroidAuto YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
